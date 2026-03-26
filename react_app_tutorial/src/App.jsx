df5ea2d9a9fb8fb3bdb4ee24929f87ca778e88cd
WILLIAM NDONI <ndoniwilliam09@gmail.com>
2026-03-26 18:55:04 +0300

    Implement BaaS using Appwrite

--- react_app_tutorial/src/App.jsx
+++ react_app_tutorial/src/App.jsx
@@ -1,8 +1,92 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import Search from "./components/Search"
+import Spinner from "./components/Spinner"
+import MovieCard from "./components/MovieCard"
+import { useDebounce } from "react-use"
+import { updateSearchCount } from "./appwrite.js"
+import { getTrendingMovies } from "./appwrite.js"
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_BASE_URL = "https://api.themoviedb.org/3";
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`
+    }
+}
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [movieList, setMovieList] = useState([]); 
+  const [isLoading, setIsLoading] = useState(false);  
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
+  const [trendingMovies, setTrendingMovies] = useState([]);
+// Debounce the search term to avoid excessive API calls while the user is typing
+  // useDebounce(() => {
+  //   setDebouncedSearchTerm(searchTerm);
+  // }, 500, [searchTerm]);
+// Native debounce implementation using useEffect and setTimeout
+  useEffect(() => {
+  const timeout = setTimeout(() => {
+    setDebouncedSearchTerm(searchTerm);
+  }, 500);
+
+  return () => clearTimeout(timeout);
+}, [searchTerm]);
+
+  const fetchMovies = async (query="") => {
+    setIsLoading(true);
+    setErrorMessage("");
+    try {
+      const endpoint = query ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}` : 
+      `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+
+      console.log("Fetched movies:", data);
+
+      if(data.Response === "False") {
+        setErrorMessage(data.Error || "No movies found.");
+        setMovieList([]);
+        return;
+      }
+      setMovieList(data.results || []);
+
+      if (query && data.results.length > 0) {
+        updateSearchCount(query, data.results[0]);
+      }
+
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+      setErrorMessage("Failed to fetch movies.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const fetchTrendingMovies = async () => {
+    try {
+      const trending = await getTrendingMovies();
+      setTrendingMovies(trending);
+    } catch (error) {
+      console.error("Error fetching trending movies:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
+
+  useEffect(() => {
+    fetchTrendingMovies();
+  }, []);
+  
   return (
     <main>
       <div className='pattern' />
@@ -10,10 +94,40 @@ const App = () => {
         <header>
           <img src="./hero.jpg" alt="Hero Banner" />
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
+        {trendingMovies.length > 0 && (
+          <section className="trending">
+            <h2>Trending Movies</h2>
+            <div className="overflow-hidden">
+            <ul className="flex gap-4 animate-scroll">
+              {trendingMovies.map((movie, index) =>(
+                <li key={movie.$id}>
+                  <p>{index + 1}</p>
+                  <img  src={movie.poster_url} alt={movie.title} />
+                </li>
+              ))}
+            </ul>
+            </div>
+            </section>
+        )}
+        
+        <section className="all-movies">
+          <h2>All Movies</h2>
 
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-        <h1 className="text-white">{searchTerm}</h1>
+          {isLoading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </ul>
+          )
+        }
+        </section>
       </div>
     </main>
   )
